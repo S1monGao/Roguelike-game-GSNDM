bd51d122a2fe61ea17c246f3a8c50a101e31ccae
S1monGao <xgao0001@student.monash.edu>
2019-05-15 17:47:51 +1000

    Yugo attempt

---
+++ src/game/YugoMaxx.java
@@ -0,0 +1,11 @@
+package game;
+import edu.monash.fit2099.engine.Actor;
+
+public class YugoMaxx extends Actor{
+
+	public YugoMaxx(String name, char displayChar, int priority, int hitPoints) {
+		super(name, displayChar, priority, hitPoints);
+		// TODO Auto-generated constructor stub
+	}
+
+}
