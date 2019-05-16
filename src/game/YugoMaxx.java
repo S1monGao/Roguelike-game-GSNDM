cab63e0ece40689c1b3370a72de45f098f4c3e19
S1monGao <xgao0001@student.monash.edu>
2019-05-16 13:53:11 +1000

    Boss fight attempt

--- src/game/YugoMaxx.java
+++ src/game/YugoMaxx.java
@@ -1,11 +1,38 @@
 package game;
 import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.IntrinsicWeapon;
 
 public class YugoMaxx extends Actor{
 
-	public YugoMaxx(String name, char displayChar, int priority, int hitPoints) {
-		super(name, displayChar, priority, hitPoints);
+	private boolean exoskeleton = true;
+	
+	public YugoMaxx(String name) {
+		super(name, 'Y', 5, 50);
 		// TODO Auto-generated constructor stub
 	}
+	
+	public boolean exoskeleton() {
+		return exoskeleton;
+	}
 
+	public void breakExoskeleton() {
+		this.exoskeleton = false;
+	}
+	
+	@Override
+	/**
+	 * Creates and returns an intrinsic weapon.
+	 *
+     * By default, the Actor 'punches' for 5 damage.  Override this method to create an Actor with
+     * more interesting descriptions and/or damage.
+     * 
+     * Yugo Maxx's attack makes 10 damages
+     *
+     * @return a freshly-instantiated IntrinsicWeapon
+     */
+	protected IntrinsicWeapon getIntrinsicWeapon() {
+		return new IntrinsicWeapon(10, "punches");
+	}
+
+	
 }
